1e1f949db3b3fd9d9aeaeb23cc20530b70086109
Sicko <mohammedzawahreh2008@gmail.com>
2025-10-27 19:50:54 +0400

    Added Eel Setup

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-# Open command prompt and run the following to ensure python enviroment is installed: python --version
-# Run this to ensure your python installation is working.
-print("Python installation is working.")
+import eel
+import os
+import shutil
+import atexit
+
+chromeDataDirectory = None
+
+eel.init("web")
+
+# Eel Core System
+class CoreSystem:
+    def exitCleanup():
+        if (os.path.exists(chromeDataDirectory)):
+            shutil.rmtree(chromeDataDirectory)
+            os.makedirs(chromeDataDirectory)
+
+    def startUp():
+        userdataDirectory = os.path.join(os.getcwd(), 'chrome-instance-user-data')
+        global chromeDataDirectory
+        chromeDataDirectory = userdataDirectory
+        if not (os.path.exists(userdataDirectory)):
+            os.makedirs(userdataDirectory)
+        eel.start("index.html", size=(800, 700), cmdline_args=["--disable-extensions", '--user-data-dir={}'.format(userdataDirectory)])
+            
+
+atexit.register(CoreSystem.exitCleanup)
+CoreSystem.startUp()
---
+++ web/defaultAll.js
@@ -0,0 +1,23 @@
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
+document.addEventListener('keydown', function(event) {
+    if ((event.key.startsWith("F") && event.key !== "f" && event.key !== "F") || event.shiftKey || event.ctrlKey || event.altKey || event.key === "Tab") {
+        //event.preventDefault();
+    }
+});
