c874cbab88b67cf5087b0c9451928ca244aebeda
Sicko <mohammedzawahreh2008@gmail.com>
2025-10-27 19:21:27 +0400

    Removed Java Backups

--- Main.java
+++
@@ -1,8 +0,0 @@
-// Open command prompt and run the following to ensure Java enviroment is installed: java -version
-// Run this to ensure your Java installation is working.
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Java installation is working.");
-    }
-}
