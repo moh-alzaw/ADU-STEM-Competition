d56976920bc330724a5152a9294d3259e0d76aaa
Sicko <mohammedzawahreh2008@gmail.com>
2025-10-27 10:03:46 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+# Open command prompt and run the following to ensure python enviroment is installed: python --version
+# Run this to ensure your python installation is working.
+print("Python installation is working.")
