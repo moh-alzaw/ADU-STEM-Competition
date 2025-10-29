f07e3cb94c09e427b329ab2f00a69702e0c092cf
khaloodi1508-stack <khaloodi1508@gmail.com>
2025-10-29 15:44:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -29,26 +29,36 @@ CoreSystem.startUp()
 
 class Exit:
     total_current_capacity = 0
+    net_movement = 0
 
-    def __init__ (self, name, current_capacity, maximum_capacity, people_leaving, people_entering):
+    def __init__ (self, name,position, current_capacity, maximum_capacity, people_leaving, people_entering):
+        self.name = name
+        self.position = position
+        self.maximum_capacity = maximum_capacity
+        self.people_leaving = people_leaving
+        self.people_entering = people_entering
+        self.current_capacity = people_leaving + people_entering
+        Exit.net_movement = people_entering - people_leaving
+        Exit.total_current_capacity += current_capacity
+
+
+
+exit1 = Exit("Exit1", (), 0, 10, 0, 0)
+exit2 = Exit("Exit2", (),0, 10, 0, 0)
+exit3 = Exit("Exit3", (),0, 10, 0, 0)
+
+class Room:
+    def __init__(self, name, current_capacity, maximum_capacity, people_leaving, people_entering):
         self.name = name
         self.current_capacity = current_capacity
         self.maximum_capacity = maximum_capacity
         self.people_leaving = people_leaving
         self.people_entering = people_entering
+        self.current_capacity = people_leaving + people_entering
+        Exit.net_movement = people_entering - people_leaving
         Exit.total_current_capacity += current_capacity
 
-
-
-exit1_position = ()
-exit2_position = ()
-exit3_position = ()
-
-class Room:
-    def __init__(self, name, current_capacity, maximum_capacity):
-        self.name = name
-        self.current_capacity = current_capacity
-        self.maximum_capacity = maximum_capacity
+main_hall = Room("Mail Hall", 0, 10)
 
 
 class Hazard:
