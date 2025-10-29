ff13591aa354812a72c9fa596b59fe7818762938
khaloodi1508-stack <khaloodi1508@gmail.com>
2025-10-29 16:12:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -41,13 +41,16 @@ class Exit:
         Exit.net_movement = people_entering - people_leaving
         Exit.total_current_capacity += current_capacity
 
-
-
-exit1 = Exit("Exit1", (), 0, 10, 0, 0)
-exit2 = Exit("Exit2", (),0, 10, 0, 0)
-exit3 = Exit("Exit3", (),0, 10, 0, 0)
+height = 10
+width = 20
+exit1 = Exit("Exit1", (0, height/2), 0, 10, 0, 0)
+exit2 = Exit("Exit2", (width, height/2),0, 10, 0, 0)
+exit3 = Exit("Exit3", (width/2, height),0, 10, 0, 0)
 
 class Room:
+    total_current_capacity = 0
+    net_movement = 0
+
     def __init__(self, name, current_capacity, maximum_capacity, people_leaving, people_entering):
         self.name = name
         self.current_capacity = current_capacity
@@ -55,17 +58,27 @@ class Room:
         self.people_leaving = people_leaving
         self.people_entering = people_entering
         self.current_capacity = people_leaving + people_entering
-        Exit.net_movement = people_entering - people_leaving
-        Exit.total_current_capacity += current_capacity
+        Room.net_movement = people_entering - people_leaving
+        Room.total_current_capacity += current_capacity
 
-main_hall = Room("Mail Hall", 0, 10)
+main_hall = Room("Mail Hall", 0, 10, 0, 0)
 
 class Person:
-    def __init__(self, position, location, room, state):
+    def __init__(self, position, room, state):
         self.position = position
-        self.location = location
         self.room = room
+        self.inside = True
         self.state = state
+        self.distance_from_nearest_exit#leaving or entering
+# accessibility thing
+    def leave(self, exit):
+        self.inside = False
+        Room.total_current_capacity -= 1
+        exit.
+    def enter(self, exit):
+        self.inside = True
+        Room.total_current_capacity += 1
+
 
 class Hazard:
     def __init__ (self, location, room, type, status): ## type is fire type
